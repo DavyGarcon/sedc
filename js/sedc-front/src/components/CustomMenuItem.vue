2e19243ad87c4aa2e1281f6598ff8bbf2486c1b6
Davy Garçon <davy.garcon@gmail.com>
2020-11-11 11:26:56 +0100

    feat(web): add base of site

---
+++ js/sedc-front/src/components/CustomMenuItem.vue
@@ -0,0 +1,61 @@
+<template>
+<md-button>
+  <md-icon>{{iconName}}</md-icon>
+  <p>{{label}}</p>
+</md-button>
+</template>
+
+<script>
+export default {
+  name: "custom-menu-item",
+  props:{
+    icon: {
+      type: String,
+      default: ""
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+  },
+  data: () => ({
+    menuVisible: false,
+    iconName: '',
+    label: ''
+  }),
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    },
+    setIconName(){
+      this.iconName = this.icon;
+    },
+    setLabel(){
+      this.label = this.title;
+    }    
+  },
+  mounted(){
+    this.setIconName();
+    this.setLabel();
+  }
+};
+</script>
+<style scoped>
+
+.md-button{
+  background-color: transparent !important;
+  box-shadow:none;
+  transition: background-color 0.8s;  
+}
+
+.md-button:hover{
+  background-color: yellow !important;
+  box-shadow:none;
+  transition: text-decoration-line 0.5s
+}
+
+.md-button:active , .md-button:focus{
+  background-color: transparent !important;  
+  text-decoration-line: underline;
+}
+</style>
---
+++ js/sedc-front/src/views/Index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="wrapper">
+    <parallax class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            <div class="brand">
+              <h1>Saint Etienne de Chigny</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="md-layout">
+        <div class="md-layout-item md-size-70 mx-auto md-small-size-100">
+          <md-card>
+            <carousel
+              :per-page="1"
+              loop
+              :speed="700"
+              autoplay              
+              :mouse-drag="false"
+              navigationEnabled
+              navigationNextLabel="<i class='material-icons'>keyboard_arrow_right</i>"
+              navigationPrevLabel="<i class='material-icons'>keyboard_arrow_left</i>"
+            >
+              <slide>
+                <div class="carousel-caption">
+                  <h4>
+                    <md-icon>location_on</md-icon>
+                    Somewhere Beyond, United States
+                  </h4>
+                </div>
+                <img :src="carousel1" alt="carousel1" />
+              </slide>
+              <slide>
+                <div class="carousel-caption">
+                  <h4>
+                    <md-icon>location_on</md-icon>
+                    Yellowstone National Park, United States
+                  </h4>
+                </div>
+                <img :src="carousel2" alt="carousel2" />
+              </slide>
+              <slide>
+                <div class="carousel-caption">
+                  <h4>
+                    <md-icon>location_on</md-icon>
+                    Yellowstone National Park, United States
+                  </h4>
+                </div>
+                <img :src="carousel3" alt="carousel3" />
+              </slide>
+            </carousel>
+          </md-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("@/assets/img/vue-mk-header.jpg")
+    }
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false,
+      carousel1: require("@/assets/img/nature-2.jpg"),
+      carousel2: require("@/assets/img/nature.jpg"),
+      carousel3: require("@/assets/img/nature-3.jpg")
+    };
+  },
+  methods: {},
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      };
+    }
+  },
+  mounted() {},
+  beforeDestroy() {}
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+</style>
